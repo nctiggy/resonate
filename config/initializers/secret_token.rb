9cb2fcc5b80c1b56832f13f59403a179865f98a9
Craig J Smith <nctiggy@gmail.com>
2014-05-29 08:02:35 -0700

    update to security token and rspec install

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Resonate::Application.config.secret_key_base = '123136173ee6e3ce11b3c2d152a791edfe50027237afdfd59366cc167016ec76eb2df1f389f9d900f70166605f21a7253d01e27aa034d2d2fd176e27bea96f01'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Resonate::Application.config.secret_key_base = secure_token
